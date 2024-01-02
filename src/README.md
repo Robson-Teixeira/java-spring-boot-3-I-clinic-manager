f83043c8de08a1e55aa20336c8b3db82e8c01985
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-01-02 16:59:45 -0300

    Criação do projeto: Spring Initializr

---
+++ src/README.md
@@ -0,0 +1,14 @@
+# java-spring-boot-3-I-clinic-manager
+
+This project was generated with [Spring Initializr](https://start.spring.io/) version 3.2.1.
+
+## Dependencies
+
+- Spring Boot DevTools
+- Lombok
+- Spring Web
+
+## Tools
+
+- [IntelliJ IDEA](https://www.jetbrains.com/pt-br/idea/);
+- [Insomnia](https://app.insomnia.rest/).
---
+++ src/main/java/br/com/alura/clinic/manager/JavaSpringBoot3IClinicManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.clinic.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBoot3IClinicManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBoot3IClinicManagerApplication.class, args);
+	}
+
+}
