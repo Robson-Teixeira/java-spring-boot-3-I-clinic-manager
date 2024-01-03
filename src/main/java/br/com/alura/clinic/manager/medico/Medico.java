ffb74b484294a93d3e12f0f4a33b35776d6e522a
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-01-03 00:16:54 -0300

    Spring Data JPA: Entidades JPA

---
+++ src/main/java/br/com/alura/clinic/manager/medico/Medico.java
@@ -0,0 +1,28 @@
+package br.com.alura.clinic.manager.medico;
+
+import br.com.alura.clinic.manager.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Medico")
+@Table(name = "medicos")
+@Getter // Geração dos métodos getters
+@NoArgsConstructor // Construtor padrão (sem argumentos) exigido pela JPA
+@AllArgsConstructor // Construtor com todos os campos/propriedades
+@EqualsAndHashCode(of = "id") // Gera métodos equals e hashCode para id
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especilidade especilidade;
+    @Embedded
+    private Endereco endereco;
+
+}
