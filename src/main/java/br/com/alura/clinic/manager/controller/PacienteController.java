02e3577d64c37884e367a2d29b91f35beb9bb18d
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-01-04 00:54:26 -0300

    Requisições GET: Listagem de pacientes

--- src/main/java/br/com/alura/clinic/manager/controller/PacienteController.java
+++ src/main/java/br/com/alura/clinic/manager/controller/PacienteController.java
@@ -1,15 +1,15 @@
 package br.com.alura.clinic.manager.controller;
 
 import br.com.alura.clinic.manager.paciente.DadosCadastroPaciente;
+import br.com.alura.clinic.manager.paciente.DadosListagemPaciente;
 import br.com.alura.clinic.manager.paciente.Paciente;
 import br.com.alura.clinic.manager.paciente.PacienteRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -22,6 +22,11 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dadosCadastroPaciente) {
         pacienteRepository.save(new Paciente(dadosCadastroPaciente));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(Pageable pageable) {
+        return pacienteRepository.findAll(pageable).map(DadosListagemPaciente::new);
     }
 
 }
---
+++ src/main/java/br/com/alura/clinic/manager/paciente/DadosListagemPaciente.java
@@ -0,0 +1,9 @@
+package br.com.alura.clinic.manager.paciente;
+
+public record DadosListagemPaciente(String nome, String email, String cpf) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+    }
+
+}
